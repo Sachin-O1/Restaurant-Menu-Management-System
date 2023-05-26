f3c3e038e40afb20969c528f2151d1a73f124523
Sachin Yadav <61198605+Sachin-O1@users.noreply.github.com>
2023-05-26 19:12:05 +0530

    Add files via upload

---
+++ restaurant_menu/views.py
@@ -0,0 +1,10 @@
+from django.views import generic
+from .models import Item
+
+class Menulist(generic.ListView):
+    queryset = Item.objects.order_by("-date_created")
+    template_name = "index.html"
+
+class MenuItemDetail(generic.DetailView):
+    model = Item
+    template_name = "menu_item_detail.html"
